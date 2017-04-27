1548a1797cba13fde4d99d18bd5ec3993feb4c25
Israel Manzo <israman814@gmail.com>
2017-04-27 08:44:38 -0400

    Updating color board

--- PhotoB&W/PhotoB&W/ViewController.swift
+++ PhotoB&W/PhotoB&W/ViewController.swift
@@ -71,6 +71,29 @@ class ViewController: UIViewController {
     }
     
     @IBAction func pickColorBtn(_ sender: Any) {
+        
+        if (sender as AnyObject).tag == 0 {
+            (red, green, blue) = (1, 0, 0)
+        } else if (sender as AnyObject).tag == 1 {
+            (red, green, blue) = (0, 1, 0)
+        } else if (sender as AnyObject).tag == 2 {
+            (red, green, blue) = (0, 0, 1)
+        } else if (sender as AnyObject).tag == 3 {
+            (red, green, blue) = (1, 0, 1)
+        } else if  (sender as AnyObject).tag == 4 {
+            (red, green, blue) = (1, 1, 0)
+        } else if (sender as AnyObject).tag == 5 {
+            (red, green, blue) = (0, 1, 1)
+            
+        } else if (sender as AnyObject).tag == 6 {
+            (red, green, blue) = (1, 1, 1)
+        } else if (sender as AnyObject).tag == 7 {
+            (red, green, blue) = (0, 0, 0)
+        }
     }
 }
+
+
+
+
 
